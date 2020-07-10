6bee6defe109e203464fb2c3356a78719524b602
Savchhenko <47991015+Savchhenko@users.noreply.github.com>
2020-07-10 11:44:48 +0300

    activate button Submit + hide warning message

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,11 @@ const modalAdd = document.querySelector(".modal__add"),
 	modalBtnSubmit = document.querySelector(".modal__btn-submit"),
 	modalSubmit = document.querySelector(".modal__submit"),
 	catalog = document.querySelector(".catalog"),
-	modalItem = document.querySelector(".modal__item");
+	modalItem = document.querySelector(".modal__item"),
+	modalBtnWarning = document.querySelector(".modal__btn-warning");
+
+const elementsModalSubmit = [...modalSubmit.elements] //получили элементы из модального окна, кроме кнопок
+	.filter((elem) => elem.tagName !== "BUTTON");
 
 const closeModal = (event) => {
 	const target = event.target;
@@ -15,17 +19,41 @@ const closeModal = (event) => {
 		modalSubmit.reset(); // сделали очистку формы
 	}
 };
+/* 
+другой вариант этой функции (через this)
+const closeModal = function(event) {
+	const target = event.target;
+	if (target.closest('.modal__close') || target === this) {
+		this.classList.add('hide');
+		if (this === modalAdd) {
+			modalSubmit.reset();
+		}
+	}
+};
+*/
 
 // закрытие модального окна по кнопке Escape
 const closeModalEsc = (event) => {
-	console.log("close");
-
 	if (event.code === "Escape") {
 		modalAdd.classList.add("hide");
 		modalItem.classList.add("hide");
-		document.removeEventListener("keydown", closeModalEsc);
+		modalSubmit.reset();
+		document.removeEventListener("keydown", closeModalEsc); // удалили это событие после его выполнения
 	}
 };
+
+modalSubmit.addEventListener("input", () => {
+	const validForm = elementsModalSubmit.every((elem) => elem.value); // если все поля ввода будут заполнены, значение поменяется на true
+	modalBtnSubmit.disabled = !validForm; // снимаем блокировку кнопки
+	modalBtnWarning.style.display = validForm ? "none" : ""; // убрали надпись "Заполните все поля"
+	/* или убрать надпись можно так:
+	if (validForm) {
+		modalBtnWarning.style.display = 'none'
+	} else {
+		modalBtnWarning.style.display = ''
+	}
+	*/
+});
 
 addAd.addEventListener("click", () => {
 	modalAdd.classList.remove("hide");
